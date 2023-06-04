1b81c9b3332b2ea58dd2a84f5fb5dc508efee6a4
I <roi972@gmail.com>
2023-06-05 00:27:23 +0300

    generate ai image on new post

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -5,6 +5,7 @@ var Comment = mongoose.model("Comment");
 var User = mongoose.model("User");
 var auth = require("../auth");
 const { sendEvent } = require("../../lib/event");
+const { createAIImage } = require("../../helpers/openai");
 
 // Preload item objects on routes with ':item'
 router.param("item", function(req, res, next, slug) {
@@ -139,7 +140,7 @@ router.get("/feed", auth.required, function(req, res, next) {
 
 router.post("/", auth.required, function(req, res, next) {
   User.findById(req.payload.id)
-    .then(function(user) {
+    .then(async function(user) {
       if (!user) {
         return res.sendStatus(401);
       }
@@ -147,6 +148,12 @@ router.post("/", auth.required, function(req, res, next) {
       var item = new Item(req.body.item);
 
       item.seller = user;
+
+      //create image from openAI if not inputted by user
+      if (!item.image) {
+        const url = await createAIImage(item.title);
+        item.image = url;
+      }
 
       return item.save().then(function() {
         sendEvent('item_created', { item: req.body.item })
